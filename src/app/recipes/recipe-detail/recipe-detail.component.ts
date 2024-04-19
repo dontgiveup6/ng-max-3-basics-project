ac5d81e4bfb46deed152835f2c9f142af06f7244
Georgi Serafimov <g.serafimov@c-trust.eu>
2024-04-19 17:55:05 +0300

    routing started + some basics added

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import {} from '../../shopping-list/shopping-list.service';
-import { Ingredient } from '../../shared/ingredient.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css',
 })
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  recipe: Recipe;
+  id: number;
+  recipeParams: Subscription;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.recipeParams = this.route.params.subscribe((params: Params) => {
+      this.recipe = this.recipeService.getRecipe(+params['id'] - 1);
+    });
+  }
+
+  ngOnDestroy() {
+    this.recipeParams.unsubscribe();
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
